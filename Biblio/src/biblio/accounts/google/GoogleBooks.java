78f6e77c531d69f795db5bf883c8bcb87ad9044e
milani.giuseppe <milani.giuseppe@53cfb31a-e9e2-99f5-1d3c-a11f2249bbb4>
2011-01-18 20:35:19 +0000

    added header gpl license

--- Biblio/src/biblio/accounts/google/GoogleBooks.java
+++ Biblio/src/biblio/accounts/google/GoogleBooks.java
@@ -1,14 +1,19 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * GoogleBooks.java
+ * This file is part of Biblio.
  *
- * Created on 2-nov-2010, 23.05.34
+ *     Biblio is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Biblio is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Biblio.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package biblio.accounts.google;
 
 import java.awt.Dimension;
